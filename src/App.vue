d56799a18ef22ce7ebe831c777581685ed48b9ea
rajendraprasadp <rajuprasadp1@gmail.com>
2021-12-11 11:35:23 +0530

    vue check

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-center items-center">
+    <div
+      class="
+        text-8xl
+        bg-green-500
+        text-center text-white
+        rounded-full
+        w-30
+        h-34
+      "
+    >
+      s
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
